2c3f78aa7f28df01d5e5b00dd77e97edab80e212
Simeão David Lamine <simeaolm@gmail.com>
2020-03-18 20:59:06 -0300

    Add on user register fuction to create a default profile

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
-import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();//initialize firebase admin
+
+/**Create default user profile when registered in the application */
+export const onUserRegister = functions.auth.user().onCreate((user) => {
+
+    const email = user.email;
+    const displayName = user.displayName;
+    const uid = user.uid;
+
+    console.log(`new user registerd: ${user.displayName} < ${user.email} >`);
+
+    return admin.firestore().collection('profiles')
+    .doc(`${email}`)
+    .set({
+        uid: uid,
+        email: email,
+        display_name: displayName,
+        city: null,
+        country: 'Brasil',
+        last_update: Date.now(),
+    });
+
+    //console.log(`Profile registered on profiles collection at ${result.writeTime}`);
+
+
+});
