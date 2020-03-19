4261f04d8cb33eff031e59c2331099a527caa115
Simeão David Lamine <simeaolm@gmail.com>
2020-03-19 19:36:47 -0300

    handle booking http request add push notification to publisher and requester refactoring getToken mechanism

--- functions/src/post_functions.ts
+++ functions/src/post_functions.ts
@@ -3,36 +3,72 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 /**Create default user profile when registered in the application */
-export const bookItem = functions.https.onRequest(async (request, response) => {  
+export const bookItem = functions.https.onRequest(async (request, response) => { 
+    console.log(JSON.stringify(request.body)); 
     const { owner, requester, postId } = request.body;
     console.log(`user ${requester} request product ${postId} from ${owner}`);
 
-    //save the intention on database
+    
+    //Get token to notify the owner&requester
+    const querySnapshot = getToken(owner);
+    const requerySnapshot = getToken(requester);
 
+    const ownerToken = (await querySnapshot).docs.map(snap => snap.id);
+    const requesterToken = (await requerySnapshot).docs.map(snap => snap.id);
+    console.log(`owner token ${ownerToken}`);
+    console.log(`requester token ${requesterToken}`);
+
+    //save the intention on database
     admin.firestore()
     .collection('donations').doc('items')
     .collection('all').doc(`${postId}`)
     .update({
         requester: requester,
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        last_update: admin.firestore.FieldValue.serverTimestamp(),
     })
-    .then((result) => {
-         //send notification to owner
-        console.log(`update result sucessfull at ${result.writeTime}`)
-        return admin.messaging().sendToDevice(
-            'registrationToken',
-            {//message payload
-                
-            },//message options
-            {
-
+    .then((result) => {//send notification to owner        
+        console.log(`update result sucessfull at ${result.writeTime}`);
+        const message: admin.messaging.MessagingPayload ={
+            notification: {
+                title: 'Nova solicitação de doação',
+                body: `${requester} está interessado no item ${postId} que você publicou.`,
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+                icon: 'your-icon-url',
             }
-
+        };
+        console.log(`Sending notification to owner ${owner}`);
+        return admin.messaging().sendToDevice(//send notification for all devices under this profile
+            ownerToken,//'registrationToken'
+            message,
         );
     })
-    .catch(error => console.log('error ocurred when requested reserve'));
+    .then(result => {//send notification to requester
+        const message: admin.messaging.MessagingPayload ={
+            notification: {
+                title: 'Confirmação de entrega',
+                body: `${requester}, sua solicitação de foi entregue ao responsável pela publicação.`,
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+                icon: 'your-icon-url',
+            }
+        };
+        console.log(`Sending notification to requester ${requester}`);
+        return admin.messaging().sendToDevice(//send notification for all devices under this profile
+            requesterToken,//'registrationToken'
+            message,
+        );
+
+    })
+    .catch(error => console.log(`error ocurred when requested reserve. ${error}`));
 
 });
+
+function getToken(profile: string){
+    return admin.firestore().collection('profiles')
+    .doc(profile)
+    .collection('tokens')
+    .get();
+
+}
 
 export const unbookItem = functions.https.onRequest((request, context) => {
     const { owner, requester, postId } = request.body;
@@ -45,6 +81,6 @@ export const unbookItem = functions.https.onRequest((request, context) => {
             .collection('all').doc(`${postId}`)
             .update({
                 requester: null,
-                timestamp: admin.firestore.FieldValue.serverTimestamp(),
+                last_update: admin.firestore.FieldValue.serverTimestamp(),
             });
 });
