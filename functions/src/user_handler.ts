1b96069a57c4885ed7d92fad1ef4ff4939b3ee36
Simeão David Lamine <simeaolm@gmail.com>
2020-03-19 19:35:45 -0300

    Change last update type from date to fieldvalue server timestamp

--- functions/src/user_handler.ts
+++ functions/src/user_handler.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-export const onUserRegistration = functions.auth.user().onCreate((user) => {
+export const onUserRegistration = functions.auth.user().onCreate((user, context) => {
 
     const email = user.email;
     const displayName = user.displayName;
@@ -17,7 +17,8 @@ export const onUserRegistration = functions.auth.user().onCreate((user) => {
         display_name: displayName,
         city: null,
         country: 'Brasil',
-        last_update: Date.now(),
+        last_update: admin.firestore.FieldValue.serverTimestamp(),
     });
 
 });
+
